f2e9713aa97d4d104a3364b86dddb2fdedbeb4aa
Kyle Hanquist <hanquist@Kyles-MacBook-Pro.local>
2025-01-03 14:19:23 -0700

    Updating to include Kn calc as well

--- Alt_Example.m
+++ Alt_Example.m
@@ -4,12 +4,17 @@
 
 % Modified by K. Hanquist - 3 Jan 2025
 
+close all
+
+% Make sure you update the characteristic dimensions (L) and Mach number if
+% you want accurate Reynolds and Knudsen numbers
+
 % The following commands creates a vector of altitudes. You can modify as
 % needed to include as many or as little altitudes as you want
 
 min_alt = 5; % km
 max_alt = 80; % km
-d_alt = 1; % km
+d_alt = 5; % km
 
 addpath('Toolbox') 
 
@@ -25,7 +30,7 @@ mu = Atmo.(10);
 % unit Reynolds number for certain flight and geometry conditions
 
 Mach = 4.9;
-D = 1; % Characteristic length or diameter [m]
+L = 1; % Characteristic length or diameter [m]
 
 % Assume constant R
 R = 287; % J/kg*K
@@ -33,9 +38,38 @@ R = 287; % J/kg*K
 % Assume constant gamma
 gamma = 1.4;
 
+% Calcualte Reynolds numbers
 for i = 1:length(altitude)
     a = sqrt(gamma * R * T(i)); % Speed of sound (m/s)
     u = Mach * a; % Velocity (m/s)
     Re_1alt(i) = rho(i) * u / mu(i); % unit Reynolds number (1/m)
-    Re_Dalt(i) = rho(i) * u * D / mu(i); % Reynolds number
+    Re_Dalt(i) = rho(i) * u * L / mu(i); % Reynolds number
 end
+
+% Calculate Knudsen number
+
+d = 4.0E-10; % Approximate hard sphere diameter of an "air" particle [m]
+
+k = 1.380649E-23; % Boltzmann constant
+
+n = p ./ (k .* T); % Number density of particles at given altitude
+
+lambda = (sqrt(2) .* n .* pi .* d^2 ).^-1; % Mean free path of particles at given altitude
+
+Kn = lambda ./ L; 
+
+% Plot the results
+yyaxis left
+semilogy(min_alt:d_alt:max_alt,Kn)
+ylabel('Kn')
+xlabel('Altitude [km]')
+
+yyaxis right
+semilogy(min_alt:d_alt:max_alt,Re_Dalt)
+ylabel('Re')
+xlabel('Altitude [km]')
+
+% Save data so we can create with Tecplot
+data = [(min_alt:d_alt:max_alt)', Kn, Re_Dalt'];
+
+save AltData.dat data -ascii
